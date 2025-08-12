2e41f05753d7a643e6d238fc4583009d5deb009c
Vipin <vipinbhardwal286@gmail.com>
2025-08-13 00:11:52 +0530

    Pattern's readme added

---
+++ patterns/tempCodeRunnerFile.java
@@ -0,0 +1,9 @@
+  // for (int i = 1; i <= n; i++) {
+        //     for (int j = i; j < n; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int k = n; k <= (2 * n - 1); k--) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
