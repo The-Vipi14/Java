5ba48796428f58e4dd652127a820d0137f56043d
Vipin <vipinbhardwal286@gmail.com>
2025-08-30 22:49:49 +0530

    Eighth pattern

---
+++ patterns/Eighth.java
@@ -0,0 +1,20 @@
+public class Eighth {
+    public static void main(String arg[]){
+        int n = 5;
+        for(int i = 1; i<=n ; i++){
+            for(int j = i; j < n; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= (2 * i - 1); j++){
+                if( j == 1 || j == (2 * i - 1) || i == n){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
